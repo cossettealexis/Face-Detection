0972b8f1f17037c2961fd85dd47758e5f3bd83e2
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-04-17 12:02:29 +0800

    Update FaceDetection.py

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,21 +1,25 @@
+import numpy as np
 import cv2
-import dlib
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 capture = cv2.VideoCapture(0)
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-while True:
-    _, frame = capture.read()
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = detector(frame)
-    cv2.imshow("Frame", frame)
 
-    for face in faces:
-        landmarks = predictor(gray_frame, face)
+while True
+    ret, image = capture.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        top_eyes = landmarks.part()
+    for(x, y, w, h) in faces:
+        cv2.rectangle(image, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = image[y:y+h, x:x+w]
 
-    key = cv2.waitKey(1)
-    if key == 27:
+    cv2.imshow('image', image)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
+
+capture.release()
+cv2.destroyAllWindows()
